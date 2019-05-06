67a93505d94ff7dbdb99eecdc6dbb9a52b54fed9
Paul <zerek81@gmail.com>
2019-05-06 14:40:09 +0200

    ecs

---
+++ Assets/BulletComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct BulletComponent : IComponentData
+{
+    public float moveSpeed;
+}
+
