adadcc5e794abaf2945d9987a17e4df1da1f73ce
Julian <julian.franz9@gmx.de>
2019-05-07 10:25:45 +0200

    whatever

---
+++ Assets/Scripts/EnemyLife.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+    public float health = 1;
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Hybrid Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class HybridTest : MonoBehaviour
+{
+    public Vector3 pos;
+}
+
+class HybridTestSystem : ComponentSystem
+{
+    struct Components
+    {
+        public Transform transform;
+       
+    }
+
+    protected override void OnUpdate()
+    {
+        //Do something for each Entity with referenced components
+        Entities.ForEach(( ref BulletComponent bulletComponent, ref EnemyPosComponent enemyPosComponent) =>
+        {
+
+            
+
+
+            
+
+        });
+    }
+}
