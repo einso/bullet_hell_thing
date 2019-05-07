5b38fa208d893095511e61fcff61a7f67853df10
Paul <zerek81@gmail.com>
2019-05-07 16:12:03 +0200

    rotatopotato OwO
    OwO

---
+++ Assets/Scripts/RotateWorld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWorld : MonoBehaviour
+{ 
+
+    public bool potato;
+
+    // Start is called before the first frame update
+    void Start()
+    { potato = true; }
+    //void rotatopotato()
+    //{
+    void Update()
+    {
+        if (potato == true)
+        {
+            Debug.Log("potatooooo!");
+            transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+            
+        }
+    }
+    //}
+
+    
+}
+
