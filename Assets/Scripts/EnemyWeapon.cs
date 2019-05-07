d9572677067ad1e5cd5115ccbe272435c9e75bd9
Paul <zerek81@gmail.com>
2019-05-07 12:06:40 +0200

    enemyshots + prefab

---
+++ Assets/Scripts/EnemyWeapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    public Transform enemyFireSpawn;
+    public GameObject EnemyProjectilePrefab;
+    private float time = 0;
+    public float firingPeriod = 1;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= firingPeriod)
+        {
+            time = 0;
+
+            Instantiate(EnemyProjectilePrefab, enemyFireSpawn.position, enemyFireSpawn.rotation);
+        }
+    }
+    // Start is called before the first frame update
+    
+
+    
+}
