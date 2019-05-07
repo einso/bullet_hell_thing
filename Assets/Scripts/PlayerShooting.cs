e68d9c7a8fa5966153d23a4608e851c4010e1790
Julian <julian.franz9@gmx.de>
2019-05-07 10:47:21 +0200

    SHooting

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public GameObject bulletSpawnPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bullet, bulletSpawnPos.transform.position, transform.rotation);
+        }
+    }
+}
