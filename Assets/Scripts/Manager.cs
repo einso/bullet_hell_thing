e4a7108e4eed9e5199aed4d505c8337af0a45df7
Paul <zerek81@gmail.com>
2019-05-07 17:09:11 +0200

    Protoboi + quit

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -23,19 +23,32 @@ public class Manager : MonoBehaviour
     {
         time = time + 1 * Time.deltaTime;
 
-        if(time > randSecNextEnemySpawn)
+        if (time > randSecNextEnemySpawn)
         {
             SpawnEnemy();
             time = 0;
             randSecNextEnemySpawn = Random.Range(minSecNextEnemySpawn, maxSecNextEnemySpawn);
             Debug.Log(randSecNextEnemySpawn);
         }
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("QUIT!");
+            Application.Quit();
+        }
     }
 
     void SpawnEnemy()
     {
         //NumberOfEnemies++;
-        float spawnPosX = Random.Range(1,21);
-        Instantiate(enemyPrefab, new Vector3(spawnPosX - 10, 0.78f, Camera.transform.position.z + 4.5f), transform.rotation);
+        float spawnPosX = Random.Range(1, 21);
+        Instantiate(enemyPrefab, new Vector3(spawnPosX - 10, 0.78f, Camera.transform.position.z + 3.7f), transform.rotation);
     }
+
+    //void Quit()
+    //{
+    //    if (Input.GetKey(KeyCode.Escape))
+    //    { Application.Quit();
+    //        Debug.Log("QUIT!");
+    //    }
+    //}
 }
