83fe331dfe1747f3865bd5285b17fa7cbb7636dd
Julian <julian.franz9@gmx.de>
2019-05-07 12:24:48 +0200

    Revert "Merge branch 'Paul0305' into Julian"
    This reverts commit d54a101053ee2ed261f2d808fed5236da165f709.

---
+++ Assets/Scripts/EnemyLife.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+    public float health = 1;
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Bullet")
+        {
+            health -= 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyPatterns.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatterns : MonoBehaviour
+{
+
+    GameObject Player;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        FollowPlayerposX(Player, 5);
+        
+    }
+
+    void FollowPlayerposX(GameObject FollowObject, float speed)
+    {
+        Vector3 FollowPos = FollowObject.transform.position;
+        Vector3 Pos = transform.position;
+
+        Debug.Log(FollowPos);
+
+        if(Pos.x  < FollowPos.x - 0.3)
+        {
+            transform.position = new Vector3(Pos.x + 1 * speed * Time.deltaTime, Pos.y, Pos.z);
+        }
+
+        if (Pos.x > FollowPos.x + 0.3)
+        {
+            transform.position = new Vector3(Pos.x - 1 * speed * Time.deltaTime, Pos.y, Pos.z);
+        }
+
+    }
+}
