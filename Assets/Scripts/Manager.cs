1de582da96a7da2a0d47490d46a6cf6654cba057
Julian <julian.franz9@gmx.de>
2019-05-07 15:33:55 +0200

    Rename

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnEnemies : MonoBehaviour
+public class Manager : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject enemyPrefab;
