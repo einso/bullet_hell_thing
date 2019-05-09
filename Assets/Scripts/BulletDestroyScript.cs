6420efd0b4f45e5416e60ee66042c7f884c64b2e
Paul <zerek81@gmail.com>
2019-05-09 15:55:11 +0200

    Poolingtestscene, poolingscripts
    works o/

---
+++ Assets/Scripts/BulletDestroyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroyScript : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Invoke("Destroy", 2f);
+    }
+
+    void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
