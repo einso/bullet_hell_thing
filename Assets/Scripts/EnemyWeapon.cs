69b416c9046b57816dc0621add0842c2b0b5c35c
Julian <julian.franz9@gmx.de>
2019-05-10 16:21:18 +0200

    New Boss Shots

--- Assets/Scripts/EnemyWeapon.cs
+++ Assets/Scripts/EnemyWeapon.cs
@@ -11,6 +11,7 @@ public class EnemyWeapon : MonoBehaviour
     public GameObject EnemyProjectilePrefab2;
     public GameObject EnemyProjectilePrefab3;
     private float time = 0;
+    float yincrease;
     public float firingPeriod = 1;
 
     public bool linearShot;
@@ -20,6 +21,7 @@ public class EnemyWeapon : MonoBehaviour
     public bool ghostShot;
     public bool duoShot;
     public bool triShot;
+    public bool flowerShot;
 
     void Start()
     {
@@ -40,7 +42,11 @@ public class EnemyWeapon : MonoBehaviour
 
         if (ghostShot) GhostShot(firingPeriod);
 
+        if (duoShot) DuoShot(firingPeriod);
+
         if (triShot) TriShot(firingPeriod);
+
+        if (flowerShot) FlowerShot(firingPeriod);
 
     }
     
@@ -109,8 +115,8 @@ public class EnemyWeapon : MonoBehaviour
     {
         if (time >= firingPeriod)
         {
-            GameObject shot = Instantiate(EnemyProjectilePrefab, enemyFireSpawn.position, enemyFireSpawn.rotation);
-            GameObject shot2 = Instantiate(EnemyProjectilePrefab, enemyFireSpawn.position, enemyFireSpawn.rotation);
+            Instantiate(EnemyProjectilePrefab, new Vector3(enemyFireSpawn.transform.position.x - 1.9f, 0.8f, enemyFireSpawn.transform.position.z), enemyFireSpawn.rotation);
+            Instantiate(EnemyProjectilePrefab, new Vector3(enemyFireSpawn.transform.position.x + 1.9f, 0.8f, enemyFireSpawn.transform.position.z), enemyFireSpawn.rotation);
             time = 0;
         }
     }
@@ -132,6 +138,26 @@ public class EnemyWeapon : MonoBehaviour
             shot3.transform.LookAt(Player);
 
             time = 0;  
+        }
+    }
+
+    void FlowerShot(float firingPeriod)
+    {
+        if (time >= firingPeriod)
+        {
+            float rotX = 0;
+            float rotY = 145 + yincrease;
+            float rotZ = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Quaternion rot = Quaternion.Euler(rotX, rotY, rotZ);
+                Instantiate(EnemyProjectilePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), rot);
+                rotY -= 45;
+            }
+
+            yincrease -= 10;
+            time = 0;
         }
     }
 
