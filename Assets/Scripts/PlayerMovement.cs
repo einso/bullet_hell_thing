e8b15a0868b584e186adf8866548c4bff75d8eb1
Julian <julian.franz9@gmx.de>
2019-05-20 13:26:48 +0200

    Changeable probabilties

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,21 +10,11 @@ public class PlayerMovement : MonoBehaviour
     public GameObject Manager;
     public float playerMoveSpeed = 15;
     public float playerRotationSpeed = 15;
-    float shipBoundary = 0.35f;
 
-    //Screenbounds
-    public Camera Camera; 
-    private Vector2 screenBounds;
-    private float objectWidth;
-    private float objectHeight;
 
     void Start()
     {
-        screenBounds = Camera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.transform.position.y));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.z; //extents = size of height / 2
 
-        Debug.Log(objectHeight);
     }
 
     private void Update()
@@ -37,12 +27,60 @@ public class PlayerMovement : MonoBehaviour
         rot = Quaternion.Euler(90, 0, 0);
         transform.rotation = rot;
 
-        //Move Player
+        //Get Movement Input
         Vector3 pos = transform.position;
-        Vector3 posChange = new Vector3(Input.GetAxisRaw("Horizontal") * playerMoveSpeed * Time.deltaTime, Input.GetAxisRaw("Vertical") * playerMoveSpeed * Time.deltaTime, 0);
+        float moveHori = Input.GetAxisRaw("Horizontal");
+        float moveVerti = Input.GetAxisRaw("Vertical");
+
+        //Set Movement Boundaries
+        if (transform.position.x < -7f)
+        {
+            if(moveHori < 0)
+            {
+                moveHori = 0;
+            }
+        }
+
+        if (transform.position.x > 8.5f)
+        {
+            if (moveHori > 0)
+            {
+                moveHori = 0;
+            }
+        }
+
+        if (transform.position.z < -0.25f)
+        {
+            if (moveVerti < 0)
+            {
+                moveVerti = 0;
+            }
+        }
+
+        if (transform.position.z > 8f)
+        {
+            if (moveVerti > 0)
+            {
+                moveVerti = 0;
+            }
+        }
+
+        //Set Player to new Position
+        Vector3 posChange = new Vector3(moveHori * playerMoveSpeed * Time.deltaTime, moveVerti * playerMoveSpeed * Time.deltaTime, 0);
         pos += rot * posChange;
         transform.position = pos;
 
+
+
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            FindObjectOfType<Manager>().PlayerDeath();
+        }
+    }
 }
