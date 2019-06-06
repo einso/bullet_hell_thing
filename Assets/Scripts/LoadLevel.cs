13c7a5d4889aa95ed74ef9de540ef2952ad0fe59
Julian <julian.franz9@gmx.de>
2019-06-06 14:08:21 +0200

    Wave

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -35,24 +35,27 @@ public class LoadLevel : MonoBehaviour
     void LoadLevel1()
     {
         //GetComponent<Manager>().amountOfProbabilities = GetComponent<Manager>().AmountOfProbabilities();
+        GetComponent<Manager>().waveSize = GetComponent<Level1>().waveSize;
         GetComponent<Manager>().enemyProbabilities = GetComponent<Level1>().enemyProbabilities;
-        GetComponent<Manager>().maxSecNextEnemySpawn = GetComponent<Level1>().maxSecNextEnemySpawn;
-        GetComponent<Manager>().minSecNextEnemySpawn = GetComponent<Level1>().minSecNextEnemySpawn;
+       //GetComponent<Manager>().maxSecNextEnemySpawn = GetComponent<Level1>().maxSecNextEnemySpawn;
+       //GetComponent<Manager>().minSecNextEnemySpawn = GetComponent<Level1>().minSecNextEnemySpawn;
     }
 
     void LoadLevel2()
     {
         //GetComponent<Manager>().amountOfProbabilities = GetComponent<Manager>().AmountOfProbabilities();
+        GetComponent<Manager>().waveSize = GetComponent<Level2>().waveSize;
         GetComponent<Manager>().enemyProbabilities = GetComponent<Level2>().enemyProbabilities;
-        GetComponent<Manager>().maxSecNextEnemySpawn = GetComponent<Level2>().maxSecNextEnemySpawn;
-        GetComponent<Manager>().minSecNextEnemySpawn = GetComponent<Level2>().minSecNextEnemySpawn;
+        //GetComponent<Manager>().maxSecNextEnemySpawn = GetComponent<Level2>().maxSecNextEnemySpawn;
+        //GetComponent<Manager>().minSecNextEnemySpawn = GetComponent<Level2>().minSecNextEnemySpawn;
     }
 
     void LoadLevel3()
     {
         //GetComponent<Manager>().amountOfProbabilities = GetComponent<Manager>().AmountOfProbabilities();
+        GetComponent<Manager>().waveSize = GetComponent<Level3>().waveSize;
         GetComponent<Manager>().enemyProbabilities = GetComponent<Level3>().enemyProbabilities;
-        GetComponent<Manager>().maxSecNextEnemySpawn = GetComponent<Level3>().maxSecNextEnemySpawn;
-        GetComponent<Manager>().minSecNextEnemySpawn = GetComponent<Level3>().minSecNextEnemySpawn;
+        //GetComponent<Manager>().maxSecNextEnemySpawn = GetComponent<Level3>().maxSecNextEnemySpawn;
+        //GetComponent<Manager>().minSecNextEnemySpawn = GetComponent<Level3>().minSecNextEnemySpawn;
     }
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -29,7 +29,7 @@ public class Manager : MonoBehaviour
     public int[] enemyProbabilities;
     [Space(20)]
 
-    public int spawnsAtOnce = 3;
+    public int waveSize = 3;
     public float maxSecNextEnemySpawn = 0f;
     public float minSecNextEnemySpawn = 0.2f;
     float randSecNextEnemySpawn;
@@ -37,6 +37,9 @@ public class Manager : MonoBehaviour
     public float scoreCount;
     [HideInInspector]
     public float levelCount = 1f;
+
+    //[HideInInspector]
+    public int WaveEnemyNr = 0;
     //public int NumberOfEnemies;
 
     // Start is called before the first frame update
@@ -47,7 +50,8 @@ public class Manager : MonoBehaviour
 
         AmountOfProbabilities();           //Set the amount of probabilities
         scoreCount = 0;
-      
+
+
     }
 
     // Update is called once per frame
@@ -59,12 +63,15 @@ public class Manager : MonoBehaviour
             time = time + 1 * Time.deltaTime;
 
             //SpawnEnemy
-            if (time > randSecNextEnemySpawn)
-            {
-                SpawnEnemy();                                                                               //Spawn enemy
-                time = 0;                                                                                   //Reset time
-                randSecNextEnemySpawn = Random.Range(minSecNextEnemySpawn, maxSecNextEnemySpawn);           //Set new random spawn time 
-            }
+            /* if (time > randSecNextEnemySpawn)
+             {
+                 SpawnEnemy();                                                                               //Spawn enemy
+                 time = 0;                                                                                   //Reset time
+                 randSecNextEnemySpawn = Random.Range(minSecNextEnemySpawn, maxSecNextEnemySpawn);           //Set new random spawn time 
+             }*/
+
+            //SpawnWave
+            WaveManagement();
 
             //GUI Update
             scoreGUI.GetComponent<TextMeshProUGUI>().text = "Score: "+scoreCount;
@@ -124,13 +131,49 @@ public class Manager : MonoBehaviour
         //instance.GetComponent<SinusoidalMove>().frequency = Random.Range(2, 15);
     }
 
-    void SpawnNumber()
+    //SpawnWaveEvent
+    void SpawnWave()
+    {
+        for (int i = 0; i < waveSize; i++)
+        {
+            SpawnEnemy();
+            WaveEnemyNr++;
+        }
+    }
+
+    void WaveManagement()
+    {
+        if(WaveEnemyNr < 1)
+        {
+
+
+            SpawnWave();
+            if (GetComponent<LoadLevel>().Level_1)
+            {
+                GetComponent<LoadLevel>().Level_1 = false;
+                GetComponent<LoadLevel>().Level_2 = true;
+            }
+            else if (GetComponent<LoadLevel>().Level_2)
+            {
+                GetComponent<LoadLevel>().Level_2 = false;
+                GetComponent<LoadLevel>().Level_3 = true;
+            }
+            else if (GetComponent<LoadLevel>().Level_3)
+            {
+                GetComponent<LoadLevel>().Level_3 = false;
+                GetComponent<LoadLevel>().Level_1 = true;
+            }
+
+        }
+    }
+
+   /* void SpawnNumber()
     {
         for (int i = 0; i < spawnsAtOnce; i++)
         {
             Invoke("SpawnEnemy", 1);
         }
-    }
+    }*/
 
     public void PlayerLevelUp()
     {
@@ -169,7 +212,10 @@ public class Manager : MonoBehaviour
         //Spawn Particle Effect
         Instantiate(DestroyEnemyParticle, new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z), Quaternion.Euler(0,0,0));
         //Instantiate(HitEnemyParticle, new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z), other.transform.rotation);
-    }
+
+        //MinusWaveNumber
+        FindObjectOfType<Manager>().WaveEnemyNr--;
+}
 
 
 
