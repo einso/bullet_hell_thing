18816d2fc63b68d00c4a7d02ec0e52d42ea19d9d
Julian <julian.franz9@gmx.de>
2019-06-18 15:48:28 +0200

    NukeVFX

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAbilities : MonoBehaviour
 {
+    public GameObject nukepng;
     bool timeSlow;
     public float manaCostTime = 2;
     public float manaCostNuke = 200;
     [HideInInspector]
     public bool nukeEnemy;
+    int transparence = 255;
+    bool nukeVFX;
+    bool nukeCD;
+    float nukeTime;
 
     // Start is called before the first frame update
     void Start()
@@ -54,6 +60,38 @@ public class PlayerAbilities : MonoBehaviour
             nukeEnemy = false;
             GetComponent<ManaBar>().manaAmount -= manaCostNuke;
         }
+
+        if(nukeEnemy)
+        {
+            nukeVFX = true;
+        }
+
+        if(nukeVFX)
+        {
+            nukeTime += 9 * Time.deltaTime;
+            nukepng.GetComponent<Image>().color = Color32.Lerp(new Color32(255,255,255,0), new Color32(255, 255, 255, 255), nukeTime); 
+
+            if(nukeTime >= 1)
+            {                
+                nukeVFX = false;
+                nukeTime = 0;
+                nukeCD = true;
+            }            
+        }
+
+        if(nukeCD)
+        {
+            nukeTime += 0.5f * Time.deltaTime;
+            nukepng.GetComponent<Image>().color = Color32.Lerp(new Color32(255, 255, 255, 255), new Color32(255, 255, 255, 0), nukeTime);
+
+            if (nukeTime >= 1)
+            {
+                nukeCD = false;
+                nukeVFX = false;
+                nukeTime = 0;
+            }
+        }
+
     }
 
     //Time Freeze
