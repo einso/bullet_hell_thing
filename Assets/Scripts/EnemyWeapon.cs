74b0b719301c40ea69e6cd602d6614d5d2f09c22
Julian <julian.franz9@gmx.de>
2019-06-19 15:14:23 +0200

    Ringe

--- Assets/Scripts/EnemyWeapon.cs
+++ Assets/Scripts/EnemyWeapon.cs
@@ -9,12 +9,16 @@ public class EnemyWeapon : MonoBehaviour
 
     PoolEnemyBullets poolEnemyBullets;
 
+    public GameObject RingeXD;
     public GameObject EnemyProjectilePrefab;
     public GameObject EnemyProjectilePrefab2;
     public GameObject EnemyProjectilePrefab3;
     private float time = 0;
     float yincrease;
+    float t = 0;
     public float firingPeriod = 1;
+
+    float angle = 0;
 
     public bool linearShot;
     public bool sinusShot;
@@ -58,11 +62,11 @@ public class EnemyWeapon : MonoBehaviour
 
         if (shotEnemyA) ShotEnemyA(firingPeriod, 20);
 
-        if (shotEnemyB) ShotEnemyB(firingPeriod, 0);
+        if (shotEnemyB) ShotEnemyB(firingPeriod);
 
         if (shotEnemyC) ShotEnemyC(firingPeriod);
 
-        if (shotEnemyD) ShotEnemyD(firingPeriod);
+        if (shotEnemyD) ShotEnemyD(firingPeriod, 0);
     }
     
     void LinearShot(float firingPeriod)
@@ -208,21 +212,27 @@ public class EnemyWeapon : MonoBehaviour
         }
     }
 
-    void ShotEnemyB(float firingPeriod, float angle)
+    void ShotEnemyB(float firingPeriod)
     {
         if(transform.parent.GetComponent<SinusoidalMove>().shootingTime)
         {
             if (time >= firingPeriod)
             {
 
-                for (int i = 0; i < 18; i++)
-                {
-                    Quaternion rot = Quaternion.Euler(enemyFireSpawn.rotation.x, -180 - angle, enemyFireSpawn.rotation.z);
-                    GameObject shot = poolEnemyBullets.pooledObjects[poolEnemyBullets.bulletNr];
-                    shot.GetComponent<EnemyBullet>().speed = 4;
-                    poolEnemyBullets.InstantiateEnemyPool(enemyFireSpawn.position, rot);
-                    angle = angle + 20;
-                }
+                /* for (int i = 0; i < 18; i++)
+                 {                    
+                     Quaternion rot = Quaternion.Euler(enemyFireSpawn.rotation.x, -180 - angle, enemyFireSpawn.rotation.z);
+                     GameObject shot = poolEnemyBullets.pooledObjects[poolEnemyBullets.bulletNr];                    
+                     shot.GetComponent<EnemyBullet>().speed = 4;
+                     poolEnemyBullets.InstantiateEnemyPool(enemyFireSpawn.position, rot);
+                     angle = angle + 20;
+                 } */
+
+                Quaternion rot = Quaternion.Euler(enemyFireSpawn.rotation.x, -180 + angle, enemyFireSpawn.rotation.z);
+                GameObject shot = Instantiate(RingeXD, enemyFireSpawn.position, rot);
+                shot.GetComponent<EnemyBullet>().speed = 4;
+                //shot.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                angle = angle + 20;
 
                 time = 0;
             }
@@ -252,23 +262,29 @@ public class EnemyWeapon : MonoBehaviour
         }
     }
 
-    void ShotEnemyD(float firingPeriod)
+    void ShotEnemyD(float firingPeriod, float angle)
     {
         if (transform.parent.GetComponent<SinusoidalMove>().shootingTime)
         {
             if (time >= firingPeriod)
             {
-                Vector3 pos = transform.position;
 
-                GameObject shot = poolEnemyBullets.pooledObjects[poolEnemyBullets.bulletNr];
-                poolEnemyBullets.InstantiateEnemyPool(new Vector3(pos.x, pos.y, pos.z), enemyFireSpawn.rotation);
-                shot.GetComponent<EnemyBullet>().speed = 2;
-                shot.transform.LookAt(Player);
+                for (int i = 0; i < 18; i++)
+                {
+                    Quaternion rot = Quaternion.Euler(enemyFireSpawn.rotation.x, -180 - angle, enemyFireSpawn.rotation.z);
+                    GameObject shot = poolEnemyBullets.pooledObjects[poolEnemyBullets.bulletNr];
+                    shot.GetComponent<EnemyBullet>().speed = 4;
+                    poolEnemyBullets.InstantiateEnemyPool(enemyFireSpawn.position, rot);
+                    angle = angle + 20;
+                }
 
                 time = 0;
             }
         }
-        else time = 0;
+        else
+        {
+            time = 0;
+        }
 
     }
 
