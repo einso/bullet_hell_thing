65d66c9f4b6027e5738a2da1db85258bc89d9693
Julian <julian.franz9@gmx.de>
2019-07-01 16:14:07 +0200

    PlayerToggle

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float blinkSpeed = 0.1f;   //BlinkSpeed
+    float amountOfBlinks = 5;
+
+    bool invincible;
+
+    public int health = 5;  //Health
+
+    //Player Collision
+    void OnTriggerEnter(Collider other)
     {
-        
+        //Check if player is invincible
+        if(!invincible)
+        {
+            //Collision with Enemy Bullet
+            if (other.gameObject.tag == "EnemyBullet")
+            {
+                TakeDamage(1);  //Damage Calculation
+                Destroy(other.gameObject); //Destroy Enemy Bullet
+            }
+
+            //Collision with Enemy
+            if (other.gameObject.tag == "Enemy")
+            {
+                TakeDamage(1);  //Damage Calculation
+            }
+        }
+
+        //Check if health is 0
+        if(health <= 0)
+        {
+            Destroy(gameObject);    //Destroy Player
+            FindObjectOfType<Manager>().PlayerDeath();  //Game Over Screen
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Damage Calculation
+    void TakeDamage(int damage)
     {
-        
+        health -= damage;   //Health - Damage
+        StartCoroutine(toggleInvincibility()); //Toggle invincibility if hit
+    }
+
+    //Blink Player after taking Damage
+    IEnumerator blinkPlayer()
+    {
+        for (int i = 0; i < amountOfBlinks; i++)
+        {
+            if(GetComponent<SpriteRenderer>().enabled == true)
+            {                
+                yield return new WaitForSeconds(blinkSpeed);
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+
+            if(GetComponent<SpriteRenderer>().enabled == false)
+            {
+                yield return new WaitForSeconds(blinkSpeed);
+                GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+    }
+
+    //Toggle Invincibility if hit
+    IEnumerator toggleInvincibility()
+    {
+        invincible = true;
+        StartCoroutine(blinkPlayer());
+
+        float invincibilityLength = blinkSpeed * amountOfBlinks * 2;
+
+        yield return new WaitForSeconds(invincibilityLength);
+        invincible = false;
     }
 }
