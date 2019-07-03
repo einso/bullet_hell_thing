abdf66b941516026c6ad53bd984fda277878c4c5
Florian <Florian.hogrebe@games-academy.de>
2019-07-03 15:20:56 +0200

    -Audio Mager Angefangen Bereitet mir noch probleme -Menü ERweitert mit settings menü -settings menü soundeinstellungen

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using ChrisTutorials.Persistent;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadScenes : MonoBehaviour
 {
+    public GameObject menue;
+    public GameObject options;
+    public GameObject Gegner1;
+    public GameObject Gegner2;
+    public GameObject Gegner3;
 
+    public AudioClip testsound;
+    public GameObject speaker;
 
+    public void Start()
+    {
+        AudioManager.Instance.Play(testsound, speaker.transform);
+    }
     //Load TitleScreen
     public void LoadScene0()
     {
@@ -23,5 +35,25 @@ public class LoadScenes : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    //Options Menü
+    public void Optionsstart()
+    {
+        menue.SetActive(false);
+        options.SetActive(true);
+        Gegner1.SetActive(false);
+        Gegner2.SetActive(false);
+        Gegner3.SetActive(false);
+    }
+
+    //options Back
+    public void Optionsback()
+    {
+        menue.SetActive(true);
+        options.SetActive(false);
+        Gegner1.SetActive(true);
+        Gegner2.SetActive(true);
+        Gegner3.SetActive(true);
     }
 }
---
+++ Assets/flo/soundManager.cs
@@ -0,0 +1,28 @@
+﻿using ChrisTutorials.Persistent;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soundManager : MonoBehaviour
+{
+    public AudioManager.AudioChannel channel1;
+    public Text soundlevelsText;
+    public string channelName = "Master Sound";
+
+    public Slider slider;
+
+    public void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void UpdateSoundLevel()
+    {
+        int sliderValue = (int)(slider.value * 100);
+        
+        AudioManager.Instance.SetVolume(channel1, sliderValue);
+
+        soundlevelsText.text = channelName + ": " + sliderValue + " / " + "100";
+    }
+}
