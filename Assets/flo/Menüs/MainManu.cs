bed82c302dbe8aeb146c933719642c5d8bd44187
florian <florian.hogrebe@games-academy.de>
2019-07-16 12:33:52 +0200

    main menu animation erstelt für rein und raus script für menu sterung angefangen

--- Assets/flo/Menüs/MainManu.cs
+++ Assets/flo/Menüs/MainManu.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class MainManu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Animatoren für die Menüs")]
+    public Animator[] animMainMenüUI;
+    public Animator[] animSettingsUI;
+    public Animator[] animSettingssoundUI;
+    public Animator[] animSettingscontrolsUI;
+    public Animator[] animSettingsgrapficUI;
+    public Animator[] animcontrolsUi;
+    public Animator protagonist;
+    int animatorlengh;
+    
+
+    [Header("Buttons Main Menü")]
+    public GameObject mainmenu;
+
+    [Header("Buttons/Slider und co Settings")]
+    public GameObject settings;
+
+    public void Update()
     {
-        
+        animatorlengh = animMainMenüUI.Length;
+    }
+    public void Settingsstart()
+    {
+        protagonist.SetBool("Raus", true);
+        for (int i = 0; i < animMainMenüUI.Length; i++)
+        {
+            animMainMenüUI[i].SetBool("Raus", true);
+        }
+
     }
 }
