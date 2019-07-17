63c94da5633bc6feae8b47db7ff13fce10f79d1b
Julian <julian.franz9@gmx.de>
2019-07-17 13:40:40 +0200

    bulletwave

---
+++ Assets/Scripts/BulletFix.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFix : MonoBehaviour
+{
+    public Manager manager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "EndWall")
+        {
+            manager.WaveEnemyNr--;
+        }
+
+    }
+}
