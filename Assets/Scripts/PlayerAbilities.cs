1cce02ee4c2156d1a110254e411b96153a7a4f28
Julian <julian.franz9@gmx.de>
2019-07-19 12:08:59 +0200

    Tank

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -36,7 +36,7 @@ public class PlayerAbilities : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {       
         if (Time.timeScale > 0 && Player.activeInHierarchy)
         {
             //NEW NUKE
