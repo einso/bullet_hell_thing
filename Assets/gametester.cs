7d84e76a53d6e55375481b50b217916e510dd4c9
Florian <Florian.hogrebe@games-academy.de>
2019-08-05 13:44:52 +0200

    higscore implementirung
    higscore ist nun implemntirt und im deatscreen sichtbar

---
+++ Assets/gametester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gametester : MonoBehaviour
+{
+    public Manager scoremanager;
+    public Text yourscore;
+    public InputField yourname;
+    public string username;
+
+    void Update()
+    {
+        yourscore.text = "" + scoremanager.scoreCount;
+        print(scoremanager.scoreCount);
+        username = yourname.text;   
+        
+    }
+    public void Loghighscore()
+    {
+
+        Higscore_dreamlo.AddNewHighscore(username, (int)scoremanager.scoreCount);
+    }
+}
