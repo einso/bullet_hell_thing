97d18e40ba660b7096b7280cc8632d9ba2d8fbaf
Florian <Florian.hogrebe@games-academy.de>
2019-08-05 14:07:18 +0200

    score multifehler behoben

--- Assets/gametester.cs
+++ Assets/gametester.cs
@@ -8,18 +8,26 @@ public class gametester : MonoBehaviour
     public Manager scoremanager;
     public Text yourscore;
     public InputField yourname;
+    public GameObject inputfield;
+    public GameObject enterbutton;
+    public GameObject usertext;
+    public Text usernametext;
     public string username;
 
     void Update()
     {
         yourscore.text = "" + scoremanager.scoreCount;
         print(scoremanager.scoreCount);
-        username = yourname.text;   
+        username = yourname.text;
+
         
     }
     public void Loghighscore()
     {
-
+        usernametext.text = yourname.text;
+        usertext.SetActive(true);
+        inputfield.SetActive(false);
+        enterbutton.SetActive(false);
         Higscore_dreamlo.AddNewHighscore(username, (int)scoremanager.scoreCount);
     }
 }
