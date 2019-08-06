dd2905909f5a37f515604ce2d3735ab34bded6e8
Julian <julian.franz9@gmx.de>
2019-08-06 14:51:35 +0200

    PlayerHoverboard

--- Assets/Scripts/EnemyLife.cs
+++ Assets/Scripts/EnemyLife.cs
@@ -54,7 +54,7 @@ public class EnemyLife : MonoBehaviour
         }
 
         if(Manager.GetComponent<PlayerAbilities>().timeSlow) CheckHealth(false); //Dont give mana when time slow is on
-        else CheckHealth(true); //give mana when timeslow is off
+        else CheckHealth(true); //give mana when timeslow is off 
     }
 
     IEnumerator PopMonster()
